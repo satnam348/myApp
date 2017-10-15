477994667df6e907ab1798e15c06cb9368cb9c25
satnam348 <satnammca@gmail.com>
2017-10-15 16:10:45 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -6,7 +6,7 @@ const app = express();
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
 app.get('*', function (req, res) {
-  res.sendfile('./dist/index.html'); // load our index.html file
+  res.sendfile('./src/index.html'); // load our index.html file
 });
 // Start the app by listening on the default
 // Heroku port
